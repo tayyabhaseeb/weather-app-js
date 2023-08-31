1684cb17e0cb9ab165914a56cc192870804acaef
Tayyab Haseeb <73613576+tayyabhaseeb@users.noreply.github.com>
2023-08-31 22:34:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const inputValue = document.getElementById("input");
+// const searchBtn = document.getElementById("searchBtn");
+
+// searchBtn.addEventListener("click", function () {
+//   //   console.log(inputValue.value);
+//   fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&units=metric&appid=750f3f5e2cda3e4f834b61b030ca035d`
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//     });
+// });
